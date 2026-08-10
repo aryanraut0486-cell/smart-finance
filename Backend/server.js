38f89f434f21a80fe9a9ad987ddda91445093f47
Aryan Raut <aryanraut0486@gmail.com>
2026-08-11 01:30:52 +0530

    Add transaction routes

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
+const transactionRoutes = require("./routes/transactionRoutes");
 
 dotenv.config();
 
@@ -13,11 +14,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Home route
 app.get("/", (req, res) => {
   res.json({
     message: "Smart Finance Backend is running 🚀",
   });
 });
+
+// Transaction routes
+app.use("/api/transactions", transactionRoutes);
 
 const PORT = process.env.PORT || 5000;
 
