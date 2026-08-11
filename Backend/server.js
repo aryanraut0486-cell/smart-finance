e332dbfae2e0a8c33cdaf53911c79924d8874684
Aryan Raut <aryanraut0486@gmail.com>
2026-08-11 20:17:02 +0530

    Add budget routes

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,7 @@ const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
+const budgetRoutes = require("./routes/budgetRoutes");
 
 dotenv.config();
 
@@ -16,20 +17,49 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Home
+// =========================
+// HOME
+// =========================
+
 app.get("/", (req, res) => {
   res.json({
     message: "Smart Finance Backend is running 🚀",
   });
 });
 
-// Authentication routes
-app.use("/api/auth", authRoutes);
+// =========================
+// AUTHENTICATION
+// =========================
 
-// Transaction routes
-app.use("/api/transactions", transactionRoutes);
+app.use(
+  "/api/auth",
+  authRoutes
+);
 
-const PORT = process.env.PORT || 5000;
+// =========================
+// TRANSACTIONS
+// =========================
+
+app.use(
+  "/api/transactions",
+  transactionRoutes
+);
+
+// =========================
+// BUDGETS
+// =========================
+
+app.use(
+  "/api/budgets",
+  budgetRoutes
+);
+
+// =========================
+// SERVER
+// =========================
+
+const PORT =
+  process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(
